65a2875a176c417d0dafdb12f0e1e596d63d8197
R.AL.F <147713433+GitVerseRALF@users.noreply.github.com>
2025-05-21 15:48:28 +0700

    Add files via upload

---
+++ src/loginandsignup/Authenticator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginandsignup;
+
+/**
+ *
+ * @author Asus
+ */
+public interface Authenticator {
+    boolean authenticate(String email, String password);
+}
+
